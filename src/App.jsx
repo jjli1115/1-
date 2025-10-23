090058285fb0f6b843241f7d923de46ef1968515
jj-li00 <lyon.li@warriorlife.net>
2025-10-22 22:19:46 -0700

    Added another division and additional text to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
+    <>
     <div className="text-3xl font-bold text-blue-600 text-center mt-10">
       🚀 Tailwind is working!
+      <h1> Testing to see if this header is working.</h1>
+      <p> Testing to see if this paragraph is working.</p>
     </div>
+    <div className="text-1xl, font-semibold text-black-300 text-center mt-5">
+      Another division.
+    </div>
+    </>
   );
 }
