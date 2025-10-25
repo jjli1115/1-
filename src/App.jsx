cd971fb43f7c49b0796db242f429bb1b222a5daa
jj-li00 <lbl53@cornell.edu>
2025-10-24 22:22:39 -0700

    Made additions to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
     <div className="text-1xl, font-semibold text-black-300 text-center mt-5">
       Another division.
       {/* Didn't get much done today, need to learn more about what makes a good coder tomorrow and also build more of the app*/}
+      {/* Learned today about the structure of an app, what each file in this project does, how they connect together, and learned more about npm syntax*/}
       <p>Paragraph testing for second division.</p>
     </div>
     </>
