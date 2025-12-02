c09849185d0e08f02007e72e76f1013295dc632e
JJ <92796599+jjli1115@users.noreply.github.com>
2025-12-01 21:30:30 -0800

    Added Comment for Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -238,3 +238,4 @@ export default function Dashboard() {
 //11.28.2025 Had dinner with Angela & Dad today, want to learn more about all the fields of AI (e.g. CoT, RNN, RLHF, etc) and review code base for The Mirror Ring
 //11.29.2025 Went shopping for Mom's birthday today and had dinner at VF, goal for tomorrow: wake up at 8a, read Meta casestudy, complete the four different tabs for 1%
 //11.30.2025 Last day of November this year, time goes fast; today I woke up at 9a, read the Meta casestudy, but did not complete the four tabs for 1%; tomorrow I will complete the four tabs for 1%
+//12.01.2025 Still didn't complete the four tabs today. Do I give up or do I push through?
