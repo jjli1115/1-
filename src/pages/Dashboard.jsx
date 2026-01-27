31fc42d574c9a5b7571ba9ee1cf95df565036313
JJ <92796599+jjli1115@users.noreply.github.com>
2026-01-27 01:14:08 -0800

    Added Comment to Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -291,3 +291,4 @@ export default function Dashboard() {
 //01.23.2026 Second day in Phoenix, it's heartwrenchingly joyous to know that grandpa Merkle lived an incredible life right to the very end and that he was loved by so many
 //01.24.2026 Third day in Phoenix, got the UHaul today and packed some of the boxes into the truck, got dinner with John and Diane (very nice people) at Texas Roadhouse, the Prime Rib and Sweet Potato (with Cinnamon and Brown Sugar) was amazing
 //01.25.2026 Introduced Sean and Noah yesterday, flew back to the Bay today
+//01.26.2026 Starting working wirh Halluminate today and went to the Quantum Computing Panel at the British Consulate where I saw Jen from Cambridge again
