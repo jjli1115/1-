4487be64c62475afe8f0dffe3538f1eb63f91f6d
JJ <92796599+jjli1115@users.noreply.github.com>
2026-09-08 23:53:47 -0400

    Added Comment to Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -514,4 +514,5 @@ export default function Dashboard() {
 //09.04.2026 Finished first week today, Mutemwa gave his MyTake (first MyTake of the section) during lunch, and drove to Cape Cod with Stefan, Sean, and Bolat
 //09.05.2026 Jared's Engagement Party at Cipriani Wall Street today!
 //09.06.2026 Got brunch with the gang at 1915 Lanzhou Hand Pulled Noodles, then drove back to Boston and dropped off Sam and Lisa on the way in Greenwich for Wes' birthday, left my suit in Sam's apartment by accident
-//09.10.2026 Labor day off today, worked and submitted GSB application (here we go)
+//09.07.2026 Labor day off today, worked on final GSB application touches (here we go)
+//09.08.2026 Had a great chat with Tyson today and he gave me many good phrases to draw from
